c17428dbbad6c01e9ddc7c251bf4481e6aab9226
AidenJN <nelsonai@oregonstate.edu>
2020-02-03 20:05:34 -0800

    Added csv reading

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -11,22 +14,26 @@ struct record {
 };
 
 struct record* add_record(string id, string name, string bio, string manager_id);
+vector<struct record> readRecords(string csvName);
 
 int main(int argc, char* argv[])
 {
     if (argc < 2) {
-        std::cerr << "Usage: " << std::endl;
-        std::cerr << "For tuple lookup: " << argv[0] << " -L [ID]" << std::endl;
-        std::cerr << "Tuple lookup example: " << argv[0] << " -L 4" << std::endl;
-        std::cerr << "For index creation: " << argv[0] << " -C" << std::endl;
+        cerr << "Usage: " << endl;
+        cerr << "For tuple lookup: " << argv[0] << " -L [ID]" << endl;
+        cerr << "Tuple lookup example: " << argv[0] << " -L 4" << endl;
+        cerr << "For index creation: " << argv[0] << " -C" << endl;
         return 1;
     }
-    std::cout << "Correct number of arguments given" << std::endl;
+    cout << "Correct number of arguments given" << endl;
 
 
-    struct record* newRecord = add_record("10000000", "Dan", "Likes dogs", "00000001");
+    //struct record* newRecord = add_record("10000000", "Dan", "Likes dogs", "00000001");
+    //cout << newRecord->bio << endl;
 
-    std::cout << newRecord->bio << std::endl;
+    vector<struct record> csvRecords = readRecords("Employee.csv");
+
+    cout << csvRecords[1].name << endl; 
 
     return 0;
 }
@@ -34,18 +41,18 @@ int main(int argc, char* argv[])
 struct record* add_record(string id, string name, string bio, string manager_id) {
 
     // Check to see if attributes are correct byte lengths
-    std::cout << "Length of id is " << id.length() << std::endl;
+    // cout << "Length of id is " << id.length() << endl;
     if (id.length() != 8) {
-        std::cerr << "ERROR: id value [" << id << "] not 8 bytes" << std::endl;
+        cerr << "ERROR: id value [" << id << "] not 8 bytes" << endl;
     }
     else if (name.length() < 1 || name.length() > 200) {
-        std::cerr << "ERROR: name value [" << name << "] not within range of 1-200 bytes" << std::endl;
+        cerr << "ERROR: name value [" << name << "] not within range of 1-200 bytes" << endl;
     }
     else if (bio.length() < 1 || bio.length() > 500) {
-        std::cerr << "ERROR: bio value [" << bio << "] not within range of 1-200 bytes" << std::endl;
+        cerr << "ERROR: bio value [" << bio << "] not within range of 1-200 bytes" << endl;
     }
     if (manager_id.length() != 8) {
-        std::cerr << "ERROR: manager_id value [" << manager_id << "] not 8 bytes" << std::endl;
+        cerr << "ERROR: manager_id value [" << manager_id << "] not 8 bytes" << endl;
     }
 
     // Create new record
@@ -59,4 +66,24 @@ struct record* add_record(string id, string name, string bio, string manager_id)
     return returnRecord; 
 }
 
+vector<struct record> readRecords(string csvName){
+    vector<struct record> records;
+    vector<string> csvAttributes;
+    string attribute; // For tokenizing csv lines
+    ifstream infile(csvName);
+
+    for(string line; getline(infile, line);)
+    {
+        //cout << line << endl;
+        stringstream linestream(line);
+        while (getline(linestream, attribute, ','))
+        {
+            //cout << attribute << endl;
+            csvAttributes.push_back(attribute);
+        }
+        records.push_back(*add_record(csvAttributes[0], csvAttributes[1], csvAttributes[2], csvAttributes[3]));
+        csvAttributes.clear();
+    }
+    return records;
+}
 
